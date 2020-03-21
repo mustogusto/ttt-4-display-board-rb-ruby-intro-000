2dc64c4ae31ee1077fb8220bed25ce9af9551081
Aisha S <a_shaheed@outlook.com>
2020-03-21 21:16:18 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,51 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board([" ", " ", " ", " ", " ", " ", " ", " ", " "])
+
+
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board[4] = "X"
+display_board([" ", " ", " ", " ", "X", " ", " ", " ", " "])
+
+board[0] = "O"
+board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board(["O", " ", " ", " ", " ", " ", " ", " ", " "])
+
+board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+board[0] = "O"
+board[4] = "X"
+display_board(["O", " ", " ", " ", "X", " ", " ", " ", " "])
+
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+board[0] = "X"
+board[1] = "X"
+board[2] = "X"
+display_board(["X", "X", "X", " ", " ", " ", " ", " ", " "])
+
+board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+display_board([" ", " ", " ", " ", " ", " ", "O", "O", "O"])
+
+board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+display_board(["X", " ", " ", " ", "X", " ", " ", " ", "X"])
+
+board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+display_board([" ", " ", "O", " ", "O", " ", "O", " ", " "])
+
+board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
+display_board(["X", "X", "X", "X", "O", "O", "X", "O", "O"])
+
+board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+display_board(["X", "O", "X", "O", "X", "X", "O", "X", "O"])
