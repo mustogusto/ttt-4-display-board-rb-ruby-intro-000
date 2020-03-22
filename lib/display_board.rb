4d699fbedab41af6ef6c9d268c8af1d222a00d55
Aisha S <a_shaheed@outlook.com>
2020-03-22 08:06:40 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -67,7 +67,7 @@ board[7] = "O"
 board[8] = "O"
 display_board([" ", " ", " ", " ", " ", " ", "O", "O", "O"])
 
-def display_board(board = [" ", " ", " ", " ", "X", " ", " ", " ", " "])
+def display_board(board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"])
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
     puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -80,11 +80,18 @@ board[4] = "X"
 board[8] = "X"
 display_board(["X", " ", " ", " ", "X", " ", " ", " ", "X"])
 
+def display_board(board = [" ", " ", "O", " ", "O", " ", "O", " ", " "])
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
 board[2] = "O"
 board{4} = "O"
 board[6] = "O"
-display_board([" ", " ", "O", " ", "O", " ", "O", " ", " "])
+
 
 def display_board(board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"])
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
