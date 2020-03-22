43d60c03dc8ca4515ba9f5dbf336f0e8d9d44cd9
Aisha S <a_shaheed@outlook.com>
2020-03-22 08:05:09 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -17,3 +17,110 @@ def display_board(board = [" ", " ", " ", " ", "X", " ", " ", " ", " "])
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+
+def display_board(board = [" ", " ", " ", " ", "X", " ", " ", " ", " "])
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+board[0] = "O"
+board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board(["O", " ", " ", " ", " ", " ", " ", " ", " "])
+
+def display_board(board = [" ", " ", " ", " ", "X", " ", " ", " ", " "])
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+board[0] = "O"
+board[4] = "X"
+display_board(["O", " ", " ", " ", "X", " ", " ", " ", " "])
+
+def display_board(board = [" ", " ", " ", " ", "X", " ", " ", " ", " "])
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+board[0] = "X"
+board[1] = "X"
+board[2] = "X"
+display_board(["X", "X", "X", " ", " ", " ", " ", " ", " "])
+
+def display_board(board = [" ", " ", " ", " ", "X", " ", " ", " ", " "])
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+board[6] = "O"
+board[7] = "O"
+board[8] = "O"
+display_board([" ", " ", " ", " ", " ", " ", "O", "O", "O"])
+
+def display_board(board = [" ", " ", " ", " ", "X", " ", " ", " ", " "])
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+board[0] = "X"
+board[4] = "X"
+board[8] = "X"
+display_board(["X", " ", " ", " ", "X", " ", " ", " ", "X"])
+
+board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+board[2] = "O"
+board{4} = "O"
+board[6] = "O"
+display_board([" ", " ", "O", " ", "O", " ", "O", " ", " "])
+
+def display_board(board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"])
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
+board[0] = "X"
+board[1] = "X"
+board[2] = "X"
+board[3] = "O"
+board[4] = "O"
+board[5] = "X"
+board[6] = "X"
+board[7] = "O"
+board[8] = "O"
+
+
+def display_board(board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"])
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+board[0] = "X"
+board[1] = "O"
+board[2] = "X"
+board[3] = "O"
+board[4] = "X"
+board[5] = "X"
+board[6] = "O"
+board[7] = "X"
+board[8] = "O"
+
+
