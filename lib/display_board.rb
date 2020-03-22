353b08d4de6a96aa1163d762987eb75afe492379
Aisha S <a_shaheed@outlook.com>
2020-03-22 07:45:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-def display_board(board)
+def display_board([" ", " ", " ", " ", " ", " ", " ", " ", " "])
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
     puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -8,7 +8,6 @@ def display_board(board)
 end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-display_board([" ", " ", " ", " ", " ", " ", " ", " ", " "])
 
 
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
