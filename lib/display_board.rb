a4750e9429ba06d90cd1ffec1ae949f07df7acdd
Aisha S <a_shaheed@outlook.com>
2020-03-22 07:58:18 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -17,5 +17,3 @@ def display_board(board = [" ", " ", " ", " ", "X", " ", " ", " ", " "])
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-
-
